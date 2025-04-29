98090927a53c2d2db957ab005403ce1f4b146409
RanRanFry <burish111@gmail.com>
2025-04-29 19:33:19 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double divide = ((double) num1 / num2) *1000;
+        int answer =(int) divide;
+        return answer;
+    }
+}
