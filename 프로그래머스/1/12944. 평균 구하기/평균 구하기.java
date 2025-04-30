83b523bf9214c924a915993b2587bed3b6a3da1d
RanRanFry <burish111@gmail.com>
2025-04-30 18:37:27 +0900

    [level 1] Title: 평균 구하기, Time: 1.09 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,10 +1,17 @@
-    class Solution {
+import java.util.Arrays;
+
+class Solution {
         public double solution(int[] arr) {
-            double answer = 0;
+           /* double answer = 0;
 
             for(int i=0; i<arr.length; i++){
                 answer += arr[i];
-            }
-            return answer/arr.length;
+            } */
+            
+            // double sum = Arrays.stream(arr).sum();
+            return Arrays.stream(arr).sum()/(double)arr.length;
+            
+                
+                
         }
     }
