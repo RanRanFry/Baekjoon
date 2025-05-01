6236bb7b710168621a4b32c0bb1ced5be6a03874
RanRanFry <burish111@gmail.com>
2025-05-01 09:40:42 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String s) {
+        
+        int len = s.length();
+        if(len % 2==1){
+            return String.valueOf(s.charAt(len/2));
+        }
+        String answer = s.substring(len/2-1, len/2+1);
+        
+        return answer;
+    }
+}
