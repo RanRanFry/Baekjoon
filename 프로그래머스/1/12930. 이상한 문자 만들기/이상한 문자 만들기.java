0723025e3187c44c24f3d353586180757b7642e9
RanRanFry <burish111@gmail.com>
2025-05-04 23:45:27 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 27.02 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,58 @@
+class Solution {
+    public String solution(String s) {
+        
+        /*
+        String [] str = s.split(" ");
+        
+        StringBuilder result = new StringBuilder();
+        for(int i=0; i<str.length; i++){
+            StringBuilder words= new StringBuilder();
+            
+            for(int j=0; j<str[i].length(); j++){
+                char c= str[i].charAt(j);
+                if( j%2==0){
+                    words.append(Character.toUpperCase(c));
+                }else{
+                     words.append(Character.toLowerCase(c));
+                }
+                
+            }
+            result.append(words);
+            if(i<str.length-1){
+                result.append(" ") ;
+            }
+            
+            
+        }
+        
+        
+        // String answer = str.join(" ");
+        return result.toString();
+        
+        */
+        
+         String[] words = s.split(" ", -1);  // 공백 여러 개도 보존
+    String result = "";
+
+    for (int i = 0; i < words.length; i++) {
+        String word = words[i];
+        String transformed = "";
+
+        for (int j = 0; j < word.length(); j++) {
+            char c = word.charAt(j);
+            if (j % 2 == 0) {
+                transformed += Character.toUpperCase(c);
+            } else {
+                transformed += Character.toLowerCase(c);
+            }
+        }
+
+        result += transformed;
+        if (i != words.length - 1) {
+            result += " ";  // 단어 사이 공백 유지
+        }
+    }
+
+    return result;
+    }
+}
